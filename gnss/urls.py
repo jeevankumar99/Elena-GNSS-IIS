d9c0b60e73e1ae8756da4615d586a2409bdb0e2b
Jeevan Kumar <rg1kumar1@gmail.com>
2022-05-10 22:55:37 +0530

    Login/Logout removed!

--- gnss/urls.py
+++ gnss/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('irnss', views.irnss, name="irnss"),
     path('qzss', views.qzss, name="qzss"),
     path('sbas', views.sbas, name="sbas"),
-    path('register', views.register, name="register"),
-    path('logout', views.logout_view, name="logout"),
-    path('login', views.login_view, name="login")
+    # path('register', views.register, name="register"),
+    # path('logout', views.logout_view, name="logout"),
+    # path('login', views.login_view, name="login")
 ]
